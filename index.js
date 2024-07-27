ac1f0d2f8d77cdb70d9ab3e7768b405a5975eebe
sdpatel4 <sdpatel4@illinois.edu>
2024-07-27 12:41:36 -0700

    Correct filter function

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function filterFreshFruits() {
 
     d3.json(jsonUrl).then(function(data) {
         data.forEach(f => {
-            freshFruits.append(f);
+            if (!f.includes(",")) {
+                freshFruits.append(f);
+            }
         });
     });
     
