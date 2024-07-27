5b4208460de0f873358ec35c504886d1eb69081a
sdpatel4 <sdpatel4@illinois.edu>
2024-07-27 12:56:05 -0700

    async behavior

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-function filterFreshFruits() {
+async function filterFreshFruits() {
     const jsonUrl = 'data.json';
     const freshFruits = [];
 
-    d3.json(jsonUrl).then(function(data) {
+    await d3.json(jsonUrl).then(function(data) {
         data.forEach(f => {
             if (!f.Fruit.includes(",")) {
                 freshFruits.push(f);
-                console.log("here");
             }
         });
     });
-
-    console.log(freshFruits);
     
     return freshFruits;
 }
